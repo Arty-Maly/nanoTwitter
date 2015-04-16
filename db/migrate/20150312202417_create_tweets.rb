81229fa40460e5fc67ded5d95858c8204737bd59
Martin S <msilberg@brandeis.edu>
2015-04-16 16:34:32 -0400

    changed tweets.user_id from string to int

--- db/migrate/20150312202417_create_tweets.rb
+++ db/migrate/20150312202417_create_tweets.rb
@@ -2,7 +2,7 @@ class CreateTweets < ActiveRecord::Migration
   def change
   	create_table :tweets do |t|
   		t.string :text
-  		t.string :user_id
+  		t.integer :user_id
   		t.timestamps
   	end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -26,7 +26,7 @@ ActiveRecord::Schema.define(version: 20150320010729) do
 
   create_table "tweets", force: :cascade do |t|
     t.string   "text"
-    t.string   "user_id"
+    t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
