568f222bde5f8e8ded435f91c5171df46ee8cac2
Martin Silberberg <msilberg@Martins-MacBook-Air.local>
2015-04-26 22:05:26 -0400

    updated unit/integration tests

--- spec/follow_mini_test.rb
+++ spec/follow_mini_test.rb
@@ -1,177 +1,177 @@
-# require 'minitest/autorun'
-# require 'rack/test'
-# require File.expand_path(File.join(File.dirname(__FILE__), "../app.rb"))
-# set :environment, :test
+require 'minitest/autorun'
+require 'rack/test'
+require_relative '../app.rb'
+set :environment, :test
 
-# describe "follow" do
-# 	include Rack::Test::Methods
+describe "follow" do
+	include Rack::Test::Methods
 	
-# 	def app
-# 		Sinatra::Application
-# 	end
+	def app
+		Sinatra::Application
+	end
 	
-#   before do
-#     User.delete_all
-# 		Relationship.delete_all
-# 		Tweet.delete_all
-#   end
+  before do
+    User.delete_all
+	Relationship.delete_all
+	Tweet.delete_all
+  end
 	
-# 	#Unit tests on follow functionality
-# 	describe "follow" do
-# 		before(:each) do
-# 			user_hash1 = {:handle => "bill", :password => "test1"}
-# 			User.create(user_hash1)
-# 			user_hash2 = {:handle => "phil", :password => "test2"}
-# 			User.create(user_hash2)
+	#Unit tests on follow functionality
+	describe "follow" do
+		before(:each) do
+			user_hash1 = {:handle => "bill", :password => "test1"}
+			User.create(user_hash1)
+			user_hash2 = {:handle => "phil", :password => "test2"}
+			User.create(user_hash2)
 			
-# 			@bill_id = User.where(handle: "bill").first.id
-# 			@phil_id = User.where(handle: "phil").first.id
-# 		end
-# 		describe "creates a relationship" do
-# 			it "creates a database entry" do
-# 				Relationship.create(follower_id: @bill_id, followed_id: @phil_id)
-# 				Relationship.where(follower_id: @bill_id, followed_id: @phil_id).nil?.must_equal false
-# 			end
-# 			it "identifies the correct id for follower" do
-# 				Relationship.create(follower_id: @bill_id, followed_id: @phil_id)
-# 				follower_id = Relationship.where(follower_id: @bill_id, followed_id: @phil_id).first.follower_id
-# 				User.find(follower_id).id.must_equal @bill_id
-# 			end
-# 			it "identifies the correct id for followed user" do
-# 				Relationship.create(follower_id: @bill_id, followed_id: @phil_id)
-# 				followed_id = Relationship.where(follower_id: @bill_id, followed_id: @phil_id).first.followed_id
-# 				User.find(followed_id).id.must_equal @phil_id
-# 			end
-# 		end
-# 		it "deletes a database entry" do
-# 			Relationship.create(follower_id: @bill_id, followed_id: @phil_id)
-# 			Relationship.where(follower_id: @bill_id, followed_id: @phil_id).first.destroy
-# 			Relationship.where(follower_id: @bill_id, followed_id: @phil_id).first.must_equal nil
-# 		end
-# 	end
+			@bill_id = User.where(handle: "bill").first.id
+			@phil_id = User.where(handle: "phil").first.id
+		end
+		describe "creates a relationship" do
+			it "creates a database entry" do
+				Relationship.create(follower_id: @bill_id, followed_id: @phil_id)
+				Relationship.where(follower_id: @bill_id, followed_id: @phil_id).first.nil?.must_equal false
+			end
+			it "identifies the correct id for follower" do
+				Relationship.create(follower_id: @bill_id, followed_id: @phil_id)
+				follower_id = Relationship.where(follower_id: @bill_id, followed_id: @phil_id).first.follower_id
+				User.find(follower_id).id.must_equal @bill_id
+			end
+			it "identifies the correct id for followed user" do
+				Relationship.create(follower_id: @bill_id, followed_id: @phil_id)
+				followed_id = Relationship.where(follower_id: @bill_id, followed_id: @phil_id).first.followed_id
+				User.find(followed_id).id.must_equal @phil_id
+			end
+		end
+		it "deletes a database entry" do
+			Relationship.create(follower_id: @bill_id, followed_id: @phil_id)
+			Relationship.where(follower_id: @bill_id, followed_id: @phil_id).first.destroy
+			Relationship.where(follower_id: @bill_id, followed_id: @phil_id).first.must_equal nil
+		end
+	end
 	
-# 	#Tests the User.follow and User.unfollow methods
-# 	describe "user" do
-# 		before(:each) do
-# 			user_hash1 = {:handle => "bill", :password => "test1"}
-# 			User.create(user_hash1)
-# 			user_hash2 = {:handle => "phil", :password => "test2"}
-# 			User.create(user_hash2)
+	#Tests the User.follow and User.unfollow methods
+	describe "user" do
+		before(:each) do
+			user_hash1 = {:handle => "bill", :password => "test1"}
+			User.create(user_hash1)
+			user_hash2 = {:handle => "phil", :password => "test2"}
+			User.create(user_hash2)
 			
-# 			@bill = User.where(handle: "bill").first
-# 			@phil = User.where(handle: "phil").first
-# 		end
+			@bill = User.where(handle: "bill").first
+			@phil = User.where(handle: "phil").first
+		end
 		
-# 		it "follows a user" do
-# 			@bill.follow(@phil)
-# 			Relationship.where(follower_id: @bill.id, followed_id: @phil.id).first.nil?.must_equal false
-# 		end
+		it "follows a user" do
+			@bill.follow(@phil)
+			Relationship.where(follower_id: @bill.id, followed_id: @phil.id).first.nil?.must_equal false
+		end
 		
-# 		it "unfollows a user" do
-# 			@bill.follow(@phil)
-# 			@bill.unfollow(@phil)
-# 			Relationship.where(follower_id: @bill.id, followed_id: @phil.id).first.must_equal nil
-# 		end
-# 	end
+		it "unfollows a user" do
+			@bill.follow(@phil)
+			@bill.unfollow(@phil)
+			Relationship.where(follower_id: @bill.id, followed_id: @phil.id).first.must_equal nil
+		end
+	end
 	
-# 	#Integration tests on follow functionality
-#   describe "http methods" do
-# 		before(:each) do
-# 			user_hash1 = {:handle => "bill", :password => "test1"}
-# 			User.create(user_hash1)
-# 			user_hash2 = {:handle => "phil", :password => "test2"}
-# 			User.create(user_hash2)
-# 			user_hash3 = {:handle => "paul", :password => "test3"}
-# 			User.create(user_hash3)
+	#Integration tests on follow functionality
+  describe "http methods" do
+		before(:each) do
+			user_hash1 = {:handle => "bill", :password => "test1"}
+			User.create(user_hash1)
+			user_hash2 = {:handle => "phil", :password => "test2"}
+			User.create(user_hash2)
+			user_hash3 = {:handle => "paul", :password => "test3"}
+			User.create(user_hash3)
 	
-# 			@bill = User.where(handle: "bill").first
-# 			@phil = User.where(handle: "phil").first
-# 			@paul = User.where(handle: "paul").first
-# 		end
+			@bill = User.where(handle: "bill").first
+			@phil = User.where(handle: "phil").first
+			@paul = User.where(handle: "paul").first
+		end
 		
-# 		describe "POST on /follow" do
-# 			it "should create a follow relationship from logged-in user to given user" do
-# 				post '/follow', {:userid => @phil.id}, { "rack.session" => {:username => "bill"}}
-# 				Relationship.where(follower_id: @bill.id, followed_id: @phil.id).first.nil?.must_equal false
-# 			end
-# 		end
+		describe "POST on /follow" do
+			it "should create a follow relationship from logged-in user to given user" do
+				post '/follow', {:userid => @phil.id}, { "rack.session" => {:username => "bill",:userid => @bill.id}}
+				Relationship.where(follower_id: @bill.id, followed_id: @phil.id).first.nil?.must_equal false
+			end
+		end
 	
-# 		describe "POST on /unfollow" do
-# 			it "should delete a follow relationship" do
-# 				Relationship.create(follower_id: @bill.id, followed_id: @phil.id)
-# 				post '/unfollow', {:userid => @phil.id}, { "rack.session" => {:username => "bill"}}
-# 				Relationship.where(follower_id: @bill.id, followed_id: @phil.id).first.must_equal nil
-# 			end
-# 		end
+		describe "POST on /unfollow" do
+			it "should delete a follow relationship" do
+				Relationship.create(follower_id: @bill.id, followed_id: @phil.id)
+				post '/unfollow', {:userid => @phil.id}, { "rack.session" => {:username => "bill",:userid => @bill.id}}
+				Relationship.where(follower_id: @bill.id, followed_id: @phil.id).first.must_equal nil
+			end
+		end
 		
-# 		describe "GET on /followers" do
-# 			it "should return a user's followers" do
-# 				Relationship.create(follower_id: @bill.id, followed_id: @phil.id)
-# 				Relationship.create(follower_id: @paul.id, followed_id: @phil.id)
-# 				get '/followers', :userid => @phil.id
-# 				last_response.must_be :ok?
-# 				last_response.body.must_include "paul"
-# 				last_response.body.must_include "bill"
-# 				last_response.body.wont_include "carl"
-# 			end
-# 		end
+		describe "GET on /followers" do
+			it "should return a user's followers" do
+				Relationship.create(follower_id: @bill.id, followed_id: @phil.id)
+				Relationship.create(follower_id: @paul.id, followed_id: @phil.id)
+				get '/followers', :userid => @phil.id
+				last_response.must_be :ok?
+				last_response.body.must_include "paul"
+				last_response.body.must_include "bill"
+				last_response.body.wont_include "carl"
+			end
+		end
 		
-# 		describe "GET on /followees" do
-# 			it "should return a user's followees" do
-# 				Relationship.create(follower_id: @phil.id, followed_id: @paul.id)
-# 				Relationship.create(follower_id: @phil.id, followed_id: @bill.id)
-# 				get '/followees', :userid => @phil.id
-# 				last_response.must_be :ok?
-# 				last_response.body.must_include "paul"
-# 				last_response.body.must_include "bill"
-# 				last_response.body.wont_include "carl"
-# 			end
-# 		end
+		describe "GET on /followees" do
+			it "should return a user's followees" do
+				Relationship.create(follower_id: @phil.id, followed_id: @paul.id)
+				Relationship.create(follower_id: @phil.id, followed_id: @bill.id)
+				get '/followees', :userid => @phil.id
+				last_response.must_be :ok?
+				last_response.body.must_include "paul"
+				last_response.body.must_include "bill"
+				last_response.body.wont_include "carl"
+			end
+		end
 		
-# 		describe "GET on /user" do
-# 			it "should display the correct number of followers" do
-# 				Relationship.create(follower_id: @bill.id, followed_id: @phil.id)
-# 				get "/user/#{@phil.id}", {:userid => @phil.id}, { "rack.session" => {:username => "bill"}}
-# 				last_response.must_be :ok?
-# 				last_response.body.must_include "Followers: 1 Users"
+		describe "GET on /user" do
+			it "should display the correct number of followers" do
+				Relationship.create(follower_id: @bill.id, followed_id: @phil.id)
+				get "/user/#{@phil.id}", {:userid => @phil.id}, { "rack.session" => {:username => "bill"}}
+				last_response.must_be :ok?
+				last_response.body.must_include "Followers: 1 Users"
 				
-# 				get "/user/#{@bill.id}", {:userid => @bill.id}, { "rack.session" => {:username => "phil"}}
-# 				last_response.must_be :ok?
-# 				last_response.body.must_include "Followers: 0 Users"
-# 			end
+				get "/user/#{@bill.id}", {:userid => @bill.id}, { "rack.session" => {:username => "phil"}}
+				last_response.must_be :ok?
+				last_response.body.must_include "Followers: 0 Users"
+			end
 			
-# 			it "should display the correct number of users being followed" do
-# 				Relationship.create(follower_id: @bill.id, followed_id: @phil.id)
-# 				get "/user/#{@bill.id}", {:userid => @bill.id}, { "rack.session" => {:username => "phil"}}
-# 				last_response.must_be :ok?
-# 				last_response.body.must_include "Following: 1 Users"
+			it "should display the correct number of users being followed" do
+				Relationship.create(follower_id: @bill.id, followed_id: @phil.id)
+				get "/user/#{@bill.id}", {:userid => @bill.id}, { "rack.session" => {:username => "phil"}}
+				last_response.must_be :ok?
+				last_response.body.must_include "Following: 1 Users"
 				
-# 				get "/user/#{@phil.id}", {:userid => @phil.id}, { "rack.session" => {:username => "bill"}}
-# 				last_response.must_be :ok?
-# 				last_response.body.must_include "Following: 0 Users"
-# 			end
+				get "/user/#{@phil.id}", {:userid => @phil.id}, { "rack.session" => {:username => "bill"}}
+				last_response.must_be :ok?
+				last_response.body.must_include "Following: 0 Users"
+			end
 			
-# 			it "should display a follow button if the logged-in user is not a follower" do
-# 				get "/user/#{@phil.id}", {:userid => @phil.id}, { "rack.session" => {:username => "bill"}}
-# 				last_response.must_be :ok?
-# 				last_response.body.wont_include "form action='/unfollow'"
-# 				last_response.body.must_include "form action='/follow'"
-# 			end
+			it "should display a follow button if the logged-in user is not a follower" do
+				get "/user/#{@phil.id}", {:userid => @phil.id}, { "rack.session" => {:username => "bill"}}
+				last_response.must_be :ok?
+				last_response.body.wont_include "form action='/unfollow'"
+				last_response.body.must_include "form action='/follow'"
+			end
 			
-# 			it "should display an unfollow button if the logged-in user is a follower" do
-# 				Relationship.create(follower_id: @bill.id, followed_id: @phil.id)
-# 				get "/user/#{@phil.id}", {:userid => @phil.id}, { "rack.session" => {:username => "bill"}}
-# 				last_response.must_be :ok?
-# 				last_response.body.wont_include "form action='/follow'"
-# 				last_response.body.must_include "form action='/unfollow'"
-# 			end
+			it "should display an unfollow button if the logged-in user is a follower" do
+				Relationship.create(follower_id: @bill.id, followed_id: @phil.id)
+				get "/user/#{@phil.id}", {:userid => @phil.id}, { "rack.session" => {:username => "bill"}}
+				last_response.must_be :ok?
+				last_response.body.wont_include "form action='/follow'"
+				last_response.body.must_include "form action='/unfollow'"
+			end
 
-# 			it "should display no follow or unfollow button if the user is not logged in" do
-# 				get "/user/#{@phil.id}", :userid => @phil.id
-# 				last_response.must_be :ok?
-# 				last_response.body.wont_include "form action='/follow'"
-# 				last_response.body.wont_include "form action='/unfollow'"
-# 			end
-# 		end
-# 	end
-# end
+			it "should display no follow or unfollow button if the user is not logged in" do
+				get "/user/#{@phil.id}", :userid => @phil.id
+				last_response.must_be :ok?
+				last_response.body.wont_include "form action='/follow'"
+				last_response.body.wont_include "form action='/unfollow'"
+			end
+		end
+	end
+end
---
+++ spec/tweet_mini_test.rb
@@ -0,0 +1,127 @@
+require 'minitest/autorun'
+require 'rack/test'
+require File.expand_path(File.join(File.dirname(__FILE__), "../app.rb"))
+set :environment, :test
+
+describe "follow" do
+	include Rack::Test::Methods
+	
+ 	def app
+ 		Sinatra::Application
+ 	end
+
+   	before(:each) do
+   		User.delete_all
+ 		Tweet.delete_all
+   		#Creates a user
+   		user_hash1 = {:handle => "bill", :password => "test1"}
+ 		User.create(user_hash1)
+		@bill = User.where(handle: "bill").first
+		@bill_id = @bill.id
+   	end
+
+ 	#Unit tests on tweet functionality
+ 	describe "tweet" do
+ 		describe "creates a tweet" do
+ 			it "creates a database entry" do
+ 				text = Faker::Hacker.say_something_smart
+ 				Tweet.create(user_id: @bill_id, text: text)
+ 				Tweet.where(user_id: @bill_id, text: text).first.nil?.must_equal false
+ 			end
+ 			it "identifies the correct id for the user who tweeted" do
+ 				tweet = Tweet.create(user_id: @bill_id, text: "")
+ 				@bill_id.must_equal tweet.user_id
+ 			end
+ 		end
+ 		it "deletes an entry in the tweet table" do
+ 			Tweet.create(user_id: @bill_id, text: "")
+ 			Tweet.where(user_id: @bill_id, text: "").first.destroy
+ 			Tweet.where(user_id: @bill_id, text: "").first.must_equal nil
+ 		end
+ 	end
+	
+ 	#Tests the User#tweet method
+ 	describe "user" do		
+ 		it "creates a tweet" do
+ 			text = Faker::Hacker.say_something_smart
+ 			@bill.tweet(text)
+ 			Tweet.where(user_id: @bill_id, text: text).first.nil?.must_equal false
+ 		end
+ 		it "identifies the correct id for the user who tweeted" do
+ 			tweet = @bill.tweet("")
+ 			User.find(tweet.user_id).must_equal @bill
+ 		end
+ 	end
+	
+	#Tests the Tweet.search_latest_tweets(text) and Tweet.search_latest_tweets_by_users(user_ids) methods
+	describe "tweet" do
+		before do
+			@phil = User.create({:handle => "phil", :password => "test2"})
+ 			@paul = User.create({:handle => "paul", :password => "test3"})
+			(1..33).each do |num|
+				@bill.tweet("bill"+num.to_s)
+			end
+			(1..33).each do |num|
+				@phil.tweet("phil"+num.to_s)
+			end
+			(1..33).each do |num|
+				@paul.tweet("paul"+num.to_s)
+			end
+			(1..2).each do |num|
+				@bill.tweet("2bill"+num.to_s)
+			end
+		end
+
+		it "returns the latest tweets with a given text in it" do
+			results = Tweet.search_latest_tweets("bill")
+			results.count.must_equal 35
+			results[0].text.must_equal "2bill2"
+			results[30].text.must_equal "bill5"
+		end
+
+		it "returns the latest tweets by a given set of user ids" do
+			results = Tweet.search_latest_tweets_by_users([@paul.id, @phil.id])
+			results.count.must_equal 66
+			results[0].text.must_equal "paul33"
+			results[33].text.must_equal "phil33"
+		end
+	end
+
+ 	#Integration tests on tweet functionality
+    describe "http methods" do
+ 		before(:each) do
+ 		end
+ 		describe "POST on /tweet_search" do
+ 			it "should display the latest tweets with a given text in them" do
+ 				text = "Hello " + Time.now.to_s
+ 				text2 = "Hello 2"
+ 				text3 = "Hello 3"
+ 				@bill.tweet(text)
+ 				@bill.tweet(text2)
+ 				@bill.tweet(text3)
+
+ 				post "/tweet_search", {:tweet_search => "Hello"}
+ 				last_response.must_be :ok?
+ 				last_response.body.must_include text
+ 				last_response.body.must_include text2
+ 				last_response.body.must_include text3
+ 			end
+
+ 			it "should not display the latest tweets that do not have the given text" do
+ 				text = "Hello " + Time.now.to_s
+ 				@bill.tweet(text)
+
+ 				post "/tweet_search", {:tweet_search => "Goodbye"}
+ 				last_response.must_be :ok?
+ 				last_response.body.wont_include text
+ 			end
+ 		end
+ 		describe "POST on /tweet" do
+ 			it "should create a tweet object" do
+ 				text = "Goodbye " + Time.now.to_s
+ 				post "/tweet", {:text => text}, { "rack.session" => {:userid => @bill_id}}
+ 				Tweet.where(text: text).first.nil?.must_equal false
+ 			end
+ 		end
+ 	end
+end
