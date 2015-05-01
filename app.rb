8b9e43c470294309c8feb49a43298362f02d03a5
Martin Silberberg <msilberg@Martins-MacBook-Air.local>
2015-05-01 02:14:36 -0400

    Final touches

--- app.rb
+++ app.rb
@@ -51,121 +51,174 @@ helpers do
 		end
 	end
 
+	#User A follows User B in both the database and the REDIS cache
+	def follow(user_a,user_b)
+		#This method deletes the follow relationship in the database
+		user_a.follow(user_b)
+
+		if REDIS.exists(user_a.handle+"_relations")
+			REDIS.sadd(user_a.handle+"_relations", params[:userid])
+		end
+		if REDIS.exists(user_a.handle+"_num_following")
+			REDIS.incr(user_a.handle+"_num_following")
+		end
+		if REDIS.exists(user_b.handle+"_num_followers")
+			REDIS.incr(user_b.handle+"_num_followers")
+		end
+	end
+	#User A unfollows User B in both the database and the REDIS cache
+	def unfollow(user_a, user_b)
+		#This method deletes the follow relationship in the database
+		user_a.unfollow(user_b)
+
+		if REDIS.exists(user_a.handle+"_relations")
+			REDIS.srem(user_a.handle+"_relations", params[:userid])
+		end
+		if REDIS.exists(user_a.handle+"_num_following")
+			REDIS.decr(user_a.handle+"_num_following")
+		end
+		if REDIS.exists(user_b.handle+"_num_followers")
+			REDIS.decr(user_b.handle+"_num_followers")
+		end
+	end
+
+	#Creates a tweet object and updates both REDIS and the database
+	def tweet(user, tweet_text)
+		name = user.handle
+		tweet = user.tweet(tweet_text)
+		#Updates the global tweet list, the list of tweets by username, and the list of tweets by username and username's followers
+		update_timeline_cache_tweets(tweet, name)
+		update_cache_personal_tweet_list(tweet, name)
+		update_global_cache_tweets(tweet, name)
+	end
 
 	#Constructs a redis object that lists the last 100 tweets 
 	def create_cached_global_timeline
-		global_tweets = Tweet.search_latest_tweets("")
-		global_tweets.each do |tweet|
-			hash = Hash.new
-			hash[:user_id] = tweet.user_id
-			hash[:handle] = tweet.handle
-			hash[:text] = tweet.text
-			hash[:created_at] = Time.at(tweet.created_at).to_s
+		if REDIS.exists("latest100") == false
+			global_tweets = Tweet.search_latest_tweets("")
+			global_tweets.each do |tweet|
+				hash = Hash.new
+				hash[:user_id] = tweet.user_id
+				hash[:handle] = tweet.handle
+				hash[:text] = tweet.text
+				hash[:created_at] = Time.at(tweet.created_at).to_s
 
-			REDIS.lpush("latest100", hash.to_json)
+				REDIS.lpush("latest100", hash.to_json)
+			end
 		end
 	end
 
 	#Constructs a redis object that lists the last 100 tweets posted by the users with a given set of timeline ids
 	#The name of the redis object is name+"_logged"
 	def create_cached_logged_in_timeline(name, timeline_ids)
-		
-		#Retrieves the last 100 followees' tweets, ordered from most recent to oldest
-		
-		followee_tweets = Tweet.search_latest_tweets_by_users(timeline_ids)
+		if REDIS.exists(name+"_timeline") == false
+			#Retrieves the last 100 followees' tweets, ordered from most recent to oldest
+			
+			followee_tweets = Tweet.search_latest_tweets_by_users(timeline_ids)
 
-		#Creates a redis object for the list of followee tweets
-		followee_tweets.each do |tweet|
-			hash = Hash.new
-			hash[:user_id] = tweet.user_id
-			hash[:handle] = tweet.handle
-			hash[:text] = tweet.text
-			hash[:created_at] = Time.at(tweet.created_at).to_s
-			REDIS.lpush(name + "_timeline", hash.to_json)
+			#Creates a redis object for the list of followee tweets
+			followee_tweets.each do |tweet|
+				hash = Hash.new
+				hash[:user_id] = tweet.user_id
+				hash[:handle] = tweet.handle
+				hash[:text] = tweet.text
+				hash[:created_at] = Time.at(tweet.created_at).to_s
+				REDIS.lpush(name + "_timeline", hash.to_json)
+			end
+			REDIS.expire(name + "_timeline", 120)
 		end
-		REDIS.expire(name + "_timeline", 120)
 	end
 	
 	#produces the list of the text and timestamps of the 100 latest tweets 
 	#for a single user and saves them as a redis object
 	#The list is named "personal_"+name
 	def create_cached_personal_tweet_list(name, user_id)
-		#Queries the database for the 100 latest tweets by the given user
-		tweets = Tweet.search_latest_tweets_by_users([user_id])
+		if REDIS.exists(name+"_personal") == false
+			#Queries the database for the 100 latest tweets by the given user
+			tweets = Tweet.search_latest_tweets_by_users([user_id])
 
-		#Constructs a REDIS object containing the text and timestamps of all the tweets by the given user
-		tweets.each do |tweet|
-			hash = Hash.new
-			hash[:text] = tweet.text
-			hash[:created_at] = Time.at(tweet.created_at).to_s
-			REDIS.lpush(name + "_personal", hash.to_json)
+			#Constructs a REDIS object containing the text and timestamps of all the tweets by the given user
+			tweets.each do |tweet|
+				hash = Hash.new
+				hash[:text] = tweet.text
+				hash[:created_at] = Time.at(tweet.created_at).to_s
+				REDIS.lpush(name + "_personal", hash.to_json)
+			end
+
+			REDIS.expire(name + "_personal", 120)
 		end
-		REDIS.expire(name + "_personal", 120)
-
 	end
 
 	#Generates a redis object tracking the number of followers for a given user id, with a
 	#unique name determined by the name parameter
 	def create_cached_follower_count(name, user_id)
-		num_followers = Relationship.count_followers(user_id)
-		REDIS.set(name+"_num_followers", num_followers)
-		REDIS.expire(name+"_num_followers", 120)
+		if REDIS.exists(name+"_num_followers") == false
+			num_followers = Relationship.count_followers(user_id)
+			REDIS.set(name+"_num_followers", num_followers)
+			REDIS.expire(name+"_num_followers", 120)
+		end
 	end
 
 	#Generates a redis object tracking the number of followees for a given user id, with a
 	#unique name determined by the name parameter
 	def create_cached_followee_count(name, user_id)
-		num_following = Relationship.count_followees(user_id)
-		REDIS.set(name+"_num_following", num_following)
-		REDIS.expire(name+"_num_following", 120)
+		if REDIS.exists(name+"_num_following") == false
+			num_following = Relationship.count_followees(user_id)
+			REDIS.set(name+"_num_following", num_following)
+			REDIS.expire(name+"_num_following", 120)
+		end
 	end
 
 	def update_global_cache_tweets(tweet, handle)
-		hash = Hash.new
-		hash[:user_id] = tweet.user_id
-		hash[:handle] = handle
-		hash[:text] = tweet.text
-		hash[:created_at] = Time.at(tweet.created_at).to_s
-		if REDIS.llen("latest100") >= 100 
-			REDIS.lpop("latest100")
+		if REDIS.exists("latest100") == true
+			hash = Hash.new
+			hash[:user_id] = tweet.user_id
+			hash[:handle] = handle
+			hash[:text] = tweet.text
+			hash[:created_at] = Time.at(tweet.created_at).to_s
+			if REDIS.llen("latest100") >= 100 
+				REDIS.lpop("latest100")
+			end
+		
+			REDIS.rpush("latest100", hash.to_json)
 		end
-	
-		REDIS.rpush("latest100", hash.to_json)
 	end
 
 	def update_timeline_cache_tweets (tweet, name)
-		hash = Hash.new
-		hash[:user_id] = tweet.user_id
-		hash[:handle] = name
-		hash[:text] = tweet.text
-		hash[:created_at] = Time.at(tweet.created_at).to_s
-		if REDIS.llen(name + "_timeline") >= 100 
-			REDIS.lpop(name + "_timeline")
+		if REDIS.exists(name+"_timeline") == true
+			hash = Hash.new
+			hash[:user_id] = tweet.user_id
+			hash[:handle] = name
+			hash[:text] = tweet.text
+			hash[:created_at] = Time.at(tweet.created_at).to_s
+			if REDIS.llen(name + "_timeline") >= 100 
+				REDIS.lpop(name + "_timeline")
+			end
+			REDIS.rpush(name + "_timeline", hash.to_json)
+			REDIS.expire(name + "_timeline", 120)
 		end
-		REDIS.rpush(name + "_timeline", hash.to_json)
-		REDIS.expire(name + "_timeline", 120)
 	end
 
 	def update_cache_personal_tweet_list (tweet, name)
-		hash = Hash.new
-		hash[:text] = tweet.text
-		hash[:created_at] = Time.at(tweet.created_at).to_s
-		if REDIS.llen(name + "_personal") >= 100 
-			REDIS.lpop(name + "_personal")
+		if REDIS.exists(name+"_personal") == true
+			hash = Hash.new
+			hash[:text] = tweet.text
+			hash[:created_at] = Time.at(tweet.created_at).to_s
+			if REDIS.llen(name + "_personal") >= 100 
+				REDIS.lpop(name + "_personal")
+			end
+			REDIS.rpush(name + "_personal", hash.to_json)		
 		end
-		REDIS.rpush(name + "_personal", hash.to_json)		
-
 	end
 
 	def create_cached_relations (follower_id, username)
-		followees = Relationship.where(follower_id: userid).pluck(:followed_id)
-		followees.each do |followee|
-			REDIS.sadd(username+"_relations", followee)
+		if REDIS.exists(username+"_relations") == false
+			followees = Relationship.where(follower_id: userid).pluck(:followed_id)
+			followees.each do |followee|
+				REDIS.sadd(username+"_relations", followee)
+			end
 		end
 	end	
-
-
-  
 end
 
 ########################################
@@ -187,16 +240,16 @@ end
 #Get method for the main page
 get "/" do 
 	#Prepares a list of tweets from all users in descending order
-	if REDIS.exists("latest100") == false
-		create_cached_global_timeline
-	end
+	create_cached_global_timeline
 
 	if login?
-		followees_relationships = Relationship.where(follower_id: userid)
+		#Creates a cached list of users that the logged-in user is following
+		create_cached_relations(userid, username)
 
-		if REDIS.exists(username+"_relations") == false
-			create_cached_relations(userid, username)
-		end
+		#Creates a chached count of the number of followers of logged-in user
+		create_cached_follower_count(username, userid)
+
+		followees_relationships = Relationship.where(follower_id: userid)
 		#Checks if a redis object containing the 100 most recent tweets by the logged-in user and its followers exists
 		#If not, a new one is constructed
 		if REDIS.exists(username+ "_timeline") == false
@@ -208,8 +261,7 @@ get "/" do
 			create_cached_logged_in_timeline(username, followees)
 		end	
 		
-		#@num_followed and @num_following display number of followers/followees of the user, respectively
-		@num_followers = Relationship.count_followers(userid)
+		#@num_following displays number of followees of the user
 		@num_following = followees_relationships.length
 		
 		erb :main
@@ -266,23 +318,11 @@ post "/tweet" do
 	user = User.find(userid)
 	tweet_text = params[:text]
 	if tweet_text.length <= 140
-		tweet = user.tweet(tweet_text)
-
-		if REDIS.exists(username+"_timeline")
-			update_timeline_cache_tweets(tweet, username)
-		end
-		
-		if REDIS.exists(username+"_personal") 
-			update_cache_personal_tweet_list(tweet, username)
-		end
-		
-		if REDIS.exists("latest100")
-			update_global_cache_tweets(tweet, username)
-		end
-
+		tweet(user, tweet_text)
 	else
 		flash[:alert] = "Please reduce the length of your tweet."
 	end
+
 	redirect "/"
 end
 
@@ -304,32 +344,26 @@ get "/user/:userid" do
 		@does_follow = false
 		profile_user_id = params[:userid].to_i
 		if login?
-			if REDIS.exists(username+"_relations") == false
-				create_cached_relations(userid, username)
-			end
+			create_cached_relations(userid, username)
+
 			if REDIS.sismember(username+"_relations", profile_user_id.to_s)
 				@does_follow = true
 			end
 		end
-		if REDIS.exists(profile_user_id.to_s+"_userid") == false 
-			user = User.find(profile_user_id).handle
-			REDIS.set(profile_user_id.to_s+"_userid", user)
-		end 
+
+		user = User.find(profile_user_id).handle
+		REDIS.set(profile_user_id.to_s+"_userid", user)
+
 		@username = REDIS.get(profile_user_id.to_s+"_userid")
+
 		#This saves to cache the 100 latest tweets from the profile user.
-		if REDIS.exists(@username+"_personal") == false
-			create_cached_personal_tweet_list(@username, profile_user_id)
-		end
+		create_cached_personal_tweet_list(@username, profile_user_id)
 
 		#This saves to cache the number of followers of the profile user
-		if REDIS.exists(@username+"_num_followers") == false
-			create_cached_follower_count(@username, profile_user_id)
-		end
+		create_cached_follower_count(@username, profile_user_id)
 		
 		#This saves to cache the number of followees of the profile user
-		if REDIS.exists(@username+"_num_following") == false
-			create_cached_followee_count(@username, profile_user_id)
-		end
+		create_cached_followee_count(@username, profile_user_id)
 		
 		erb :look, :locals => {:profile_user_id => params[:userid]}
 	end
@@ -338,9 +372,9 @@ end
 #This method creates a follow relationship between the session user and the user being followed.
 post "/follow" do
 	followed = User.find(params[:userid])
-	#This method creates the follow relationship in the database
-  	User.find(userid).follow(followed)
-  	REDIS.sadd(username+"_relations", params[:userid])
+	logged_in_user = User.find(userid)
+ 
+ 	follow(logged_in_user,followed)
    
   	flash[:notice] = "You are following " + followed.handle
   	redirect "/user/#{params[:userid]}"
@@ -349,10 +383,10 @@ end
 #This method deletes a follow relationship between the session user and a given user.
 post "/unfollow" do
 	followed = User.find(params[:userid])
-	#This method deletes the follow relationship in the database
-	User.find(userid).unfollow(followed)
-	REDIS.srem(username+"_relations", params[:userid])
+	logged_in_user = User.find(userid)
  
+ 	unfollow(logged_in_user,followed)
+
   	flash[:notice] = "You are no longer following " + followed.handle
   	redirect "/user/#{params[:userid]}"
 end
@@ -384,23 +418,19 @@ get "/test_setup" do
 	if User.where(handle: "test_user").first == nil
 		tester = User.new({:handle => "test_user", :password => "1"})
 		tester.save
-		puts "Tester created!"
-		
 	end
 	erb :test_uris
 end
 
 #Creates a test tweet posted by test_user
-post "/test_tweet" do
+get "/test_tweet" do
 	text = Faker::Hacker.say_something_smart
-	tweet = User.where(handle: "test_user").first.tweet(text)
-	update_global_cache_tweets(tweet, "test_user")
-	update_timeline_cache_tweets(tweet, "test_user")
+	tweet(User.where(handle: "test_user").first, text)
+	redirect "/"
 end
 
-
 #Logs test_user in and redirects to the logged-in main root page.
-post "/test_main" do
+get "/test_main" do
 	user = User.where(handle: "test_user").first
 	session[:username] = "test_user"
 	session[:userid] = user.id
@@ -408,34 +438,35 @@ post "/test_main" do
 end
 
 #Has test_user follow/unfollow somebody at random
-post "/test_follow" do
+get "/test_follow" do
 	#Finds test_user's id
 	tester = User.where(handle: "test_user").first
 	test_id = tester.id
 
 	#Selects a random user
-	followee = User.all.shuffle.first
+	followee = User.find(rand(1..1001))
 	followee_id = followee.id
 
 	#Determines what to do to the user depending on which user it is
 
 	#If the test_user is following the user, unfollow
 	if tester.active_relationships.where(followed_id: followee_id).first != nil
-		tester.unfollow(followee)
-	#Elsif the test_user is not trying to follow itself, follow 
-	elsif followee_id != test_id
-		tester.follow(followee)
+		unfollow(tester,followee)
+	#Else follow 
+	else
+		follow(tester,followee)
 	end
+	redirect "/user/1002"
 end
 
 #Resets the database after the test has concluded
-post "/reset" do
+get "/reset" do
 	#Removes all of test_user's tweets
 	tester_id = User.where(handle: "test_user").first
 	Tweet.where(user_id: tester_id).delete_all
 	#Removes all of test_user's follows
 	Relationship.where(follower_id: tester_id).delete_all
 	REDIS.flushdb
-
+	redirect "/user/1002"
 end
 
